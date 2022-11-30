eb7422b3d8f262c51a6afadbaaac911bf5a4c0c9
Jason Bock <jason.r.bock@outlook.com>
2022-11-29 20:55:14 -0600

    .NET version update

--- src/BigNumbers/Program.cs
+++ src/BigNumbers/Program.cs
@@ -2,97 +2,97 @@
 using System.Numerics;
 
 PerformCalculationsWithLongs();
-Console.Out.WriteLine();
+Console.WriteLine();
 PerformCalculationsWithDoubles();
-Console.Out.WriteLine();
+Console.WriteLine();
 PerformCalculationsWithDecimals();
-Console.Out.WriteLine();
+Console.WriteLine();
 PerformCalculationsWithBigInteger();
-Console.Out.WriteLine();
+Console.WriteLine();
 ParseTotalAtoms();
-Console.Out.WriteLine();
+Console.WriteLine();
 AddTwoFloatingPoints();
-Console.Out.WriteLine();
+Console.WriteLine();
 PerformCalculationsWithInts();
 
-// https://docs.microsoft.com/en-us/dotnet/api/system.int64?view=net-5.0#remarks
+// https://docs.microsoft.com/en-us/dotnet/api/system.int64
 static void PerformCalculationsWithLongs()
 {
-	Console.Out.WriteLine(nameof(PerformCalculationsWithLongs));
+	Console.WriteLine(nameof(PerformCalculationsWithLongs));
 	var house = 350_000_000L;
-	var salary = 177_000_000_000L;
+	var salary = 219_000_000_000L;
 
 	var numberOfHouses = salary / house;
 
-	Console.Out.WriteLine(numberOfHouses);
+	Console.WriteLine(numberOfHouses);
 }
 
-// https://docs.microsoft.com/en-us/dotnet/api/system.double?view=net-5.0#remarks
+// https://docs.microsoft.com/en-us/dotnet/api/system.double
 static void PerformCalculationsWithDoubles()
 {
-	Console.Out.WriteLine(nameof(PerformCalculationsWithDoubles));
+	Console.WriteLine(nameof(PerformCalculationsWithDoubles));
 	var house = 350_000_000d;
-	var salary = 177_000_000_000d;
+	var salary = 219_000_000_000d;
 
 	var numberOfHouses = salary / house;
 
-	Console.Out.WriteLine(numberOfHouses);
+	Console.WriteLine(numberOfHouses);
 }
 
-// https://docs.microsoft.com/en-us/dotnet/api/system.decimal?view=net-5.0#remarks
+// https://docs.microsoft.com/en-us/dotnet/api/system.decimal
 // "The Decimal value type is appropriate for financial calculations
 // that require large numbers of significant integral and fractional digits and no round-off errors."
 static void PerformCalculationsWithDecimals()
 {
-	Console.Out.WriteLine(nameof(PerformCalculationsWithDecimals));
+	Console.WriteLine(nameof(PerformCalculationsWithDecimals));
 	var house = 350_000_000M;
-	var salary = 177_000_000_000M;
+	var salary = 219_000_000_000M;
 
 	var numberOfHouses = salary / house;
 
-	Console.Out.WriteLine(numberOfHouses);
+	Console.WriteLine(numberOfHouses);
 }
 
-// https://docs.microsoft.com/en-us/dotnet/api/system.numerics.biginteger?view=net-5.0#remarks
+// https://docs.microsoft.com/en-us/dotnet/api/system.numerics.biginteger
 static void PerformCalculationsWithBigInteger()
 {
-	Console.Out.WriteLine(nameof(PerformCalculationsWithBigInteger));
+	Console.WriteLine(nameof(PerformCalculationsWithBigInteger));
 	var house = new BigInteger(350_000_000L);
-	var salary = new BigInteger(177_000_000_000L);
+	var salary = new BigInteger(219_000_000_000L);
 
 	var numberOfHouses = salary / house;
 
-	Console.Out.WriteLine(numberOfHouses);
+	Console.WriteLine(numberOfHouses);
 }
 
 static void ParseTotalAtoms()
 {
-	Console.Out.WriteLine(nameof(ParseTotalAtoms));
+	Console.WriteLine(nameof(ParseTotalAtoms));
 	var atoms = BigInteger.Parse("100000000000000000000000000000000000000000000000000000000000000000000000000000000");
-	Console.Out.WriteLine(atoms);
+	Console.WriteLine(atoms);
 
-	var decimalAtoms = 1.0E80;
-	Console.Out.WriteLine(decimalAtoms);
+	var doubleAtoms = 1.0E80;
+	Console.WriteLine(doubleAtoms);
 
-	decimalAtoms = 1.068742959837859731897589371895738917498731894789317849731897E80;
-	Console.Out.WriteLine(decimalAtoms);
+	doubleAtoms = 1.068742959837859731897589371895738917498731894789317849731897E80;
+	Console.WriteLine(doubleAtoms);
 }
 
 // https://0.30000000000000004.com/
 static void AddTwoFloatingPoints()
 {
-	Console.Out.WriteLine(nameof(AddTwoFloatingPoints));
-	Console.Out.WriteLine(0.1 + 0.2);
-	Console.Out.WriteLine(0.1M + 0.2M);
+	Console.WriteLine(nameof(AddTwoFloatingPoints));
+	Console.WriteLine(0.1 + 0.2);
+	Console.WriteLine(0.1M + 0.2M);
 }
 
 static void PerformCalculationsWithInts()
 {
-	Console.Out.WriteLine(nameof(PerformCalculationsWithInts));
+	Console.WriteLine(nameof(PerformCalculationsWithInts));
 	var numberOfHouses = 505;
 	var house = 350_000_000;
 
 	var salary = numberOfHouses * house;
 
-	Console.Out.WriteLine(salary);
+	Console.WriteLine(salary);
 }
